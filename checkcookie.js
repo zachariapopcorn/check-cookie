d0d512f1e637d2cb014c6b775fc5c447b17bb934
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-06-10 14:27:25 -0700

    Update checkcookie.js

--- checkcookie.js
+++ checkcookie.js
@@ -3,7 +3,7 @@ const roblox = require('noblox.js');
 exports.run = async (client, message, args) => {
     let cookie = client.config.cookie;
     try {
-        await roblox.cookieLogin(cookie);
+        await roblox.setCookie(cookie);
     } catch {
         return message.channel.send("The cookie is invalid!");
     }
